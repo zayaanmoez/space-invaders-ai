44f8ea705a2412dd7762e44416e8e56455d65e17
Zayaan Moez <zayaanmoez@outlook.com>
2021-11-30 23:30:21 -0500

    DQN agent and CNN model

--- spaceInvadersAI.py
+++ spaceInvadersAI.py
@@ -4,12 +4,27 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 import numpy as np
 from collections import deque
+from itertools import islice
 
-env = gym.make('ALE/SpaceInvaders-v5', render_mode='human')
-env.reset()
 
-state_shape = env.observation_space.shape
-action_shape = env.action_space.n
+#################################################################################
+# Hyperparameters
+
+BATCH_SIZE = 64
+REPLAY_SIZE = 1000
+EPISODES = 300
+TARGET_MODEL_UPDATE = 100
+REPLAY_MEMORY = 100_000
+
+# CNN model params
+LEARNING_RATE = 0.001
+KERNEL_SIZE = 3
+POOL_SIZE = 2
+
+# Q-learning params
+Q_LEARNING_RATE = 0.5
+DISCOUNT_FACTOR = 0.5
+
 
 #################################################################################
 # Preprocessing
@@ -39,7 +54,7 @@ def preprocess(obs, normalize=False):
     print("after: ", img.shape)
 
     # reshape to 1D tensor
-    return img.reshape(85,80)
+    return img.reshape(85,80,1)
 
 # frame stacking
 # https://danieltakeshi.github.io/2016/11/25/frame-skipping-and-preprocessing-for-deep-q-networks-on-atari-2600-games/
@@ -79,59 +94,170 @@ def preproces_plot():
         plt.imshow(obs)
         f.add_subplot(1,2,2)
         plt.imshow(preprocess(obs), cmap='gray')
-        # plt.show(block=True)
+        plt.show(block=True)
 
         # env.render()
-        action = env.action_space.sample()
-        obs, reward, done, info = env.step(action)
+        # action = env.action_space.sample()
+        # obs, reward, done, info = env.step(action)
+
+
 
 #################################################################################
-# Model
+# Model - Create a convolutional neural network with Keras
 
-def agent(state_shape, action_shape):
-
-    learning_rate = 0.001
+def network(state_shape, action_shape):
     
-    initializer = keras.intialiazers.HeUniform()
+    initializer = keras.initializers.HeUniform()
     model = keras.Sequential()
-    model.add(keras.layers.Dense(24, input_shape=state_shape, activation='relu', 
-        kernel_initializer=initializer))
-    model.add(keras.layers.Dense(24, input_shape=state_shape, activation='relu', kernel_initializer=init))
-    model.add(keras.layers.Dense(12, activation='relu', kernel_initializer=init))
-    model.add(keras.layers.Dense(action_shape, activation='linear', kernel_initializer=init))
-    model.compile(loss=keras.losses.Huber(), optimizer=keras.optimizers.Adam(lr=learning_rate), metrics=['accuracy'])
+
+    # Input layer
+    model.add(keras.layers.Conv2D(32, kernel_size=KERNEL_SIZE, input_shape=state_shape, activation='relu', 
+        padding='same', kernel_initializer=initializer))
+    model.add(keras.layers.AveragePooling2D(pool_size=POOL_SIZE))
+
+    # Hidden convolutional layers
+    model.add(keras.layers.Conv2D(64, kernel_size=KERNEL_SIZE, input_shape=state_shape, activation='relu', 
+        padding='same', kernel_initializer=initializer))
+    model.add(keras.layers.AveragePooling2D(pool_size=POOL_SIZE))
+    model.add(keras.layers.Conv2D(64, kernel_size=KERNEL_SIZE, input_shape=state_shape, activation='relu', 
+        padding='same', kernel_initializer=initializer))
+    model.add(keras.layers.AveragePooling2D(pool_size=POOL_SIZE))
+
+    # Flatten and use fully connected network
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(1024, activation='relu', kernel_initializer=initializer))
+
+    # Output layer
+    model.add(keras.layers.Dense(action_shape, activation='softmax', kernel_initializer=initializer))
+
+    model.compile(loss=keras.losses.Huber(), optimizer=keras.optimizers.Adam(learning_rate=LEARNING_RATE), metrics=['accuracy'])
+    
     return model
+
+
 
 #################################################################################
 # Training agent
 
-def train():
-    episodes = 10
-    for episode in range(episodes):
-        env.reset()
+def train(env, replay_memory, model, target_model):
+
+    replay_len = len(replay_memory)
+
+    if len(replay_memory) <= REPLAY_SIZE:
+        return
+
+    batch = random.sample(replay_memory, BATCH_SIZE)
+    states = np.array(step[0] for step in batch)
+    q_values = model.predict(states)
+    succesive_states = np.array(step[3] for step in batch)
+    succesive_q_values = target_model.predict(succesive_states)
+
+    X_train = []
+    Y_train = []
+
+    for i, (state, action, reward, new_state, done) in enumerate(batch):
+        if not done:
+            # Bellman Equation : r(s) + gamma * max_a'(Q(s',a'))
+            qValue = reward + DISCOUNT_FACTOR * np.max(succesive_q_values[i])
+        else:
+            # Pick reward as the episode has ended; no succesive state
+            qValue = reward
+        
+        # TODO: Figure out y_train values work or not
+        # Temporal Difference
+        # q_value_arr for a state s : [qVal action1, qval action1, ..., qval action18] 
+        q_value_arr = q_values[i]
+        # Qvalue for action a  : Q(s,a) + alpha(r(s) + gamma*max_a'(Q(s',a')) - Q(s, a))         
+        q_value_arr[action] = q_value_arr[action] + LEARNING_RATE(qValue - q_value_arr[action])
+
+        X_train.append(state)
+        Y_train.append()
+    
+    model.fit(np.array(X_train), np.array[Y_train], batch_size=BATCH_SIZE)
+
+
+
+#################################################################################
+# Deep Q-Learning agent
+
+def DQN_agent(env):
+    epsilon = 1
+    decay = 0.01
+
+    model = network(state_shape, action_shape)
+    target_model = network(state_shape, action_shape)
+    target_model.set_weights(model.get_weights())
+
+    # Memory buffer to store the last N experiences
+    replay_memory = deque(maxlen=REPLAY_MEMORY)
+
+    update_target_counter = 0
+    step_counter = 0
+
+    for episode in range(EPISODES):
+        state = env.reset()
         score = 0 
         done = False
 
         while not done:
-            action = env.action_space.sample()
-            n_state, reward, done, info = env.step(action)
-            score+=reward
-        print('Episode:{} Score:{}'.format(episode, score))
+            step_counter += 1
+
+            # Epsilon Greedy Strategy with explore probability epsilon
+            if np.random.rand() <= epsilon:
+                # Explore 
+                action = env.action_space.sample()
+            else:
+                # Exploit best action from cnn
+                preprocessed_state = preprocess(state) # Preprocess state
+                predictions = model.predict(preprocessed_state).flatten()
+                action = np.argmax(predictions)
+            
+            new_state, reward, done, info = env.step(action)
+            replay_memory.append([preprocess(state), action, reward, preprocess(new_state), done]) # ERROR CHECK: When done cannot preprocess next state
+
+            if step_counter % 4 == 0 or done:
+                train(env, replay_memory, model, target_model)
+
+            score += reward
+            state = new_state
+
+            if update_target_counter >= TARGET_MODEL_UPDATE:
+                    update_target_counter = 0
+                    target_model.set_weights(model.get_weights())
+
+            if done:
+                  print('Score: {} after epsidoe = {} and final reward = {}'.format(score, episode, reward))
+            
+        # Exponetial decay for epsilon (explore with atleast 0.01 or 1% probability)
+        epsilon = 0.01 + 0.99 * np.exp(-decay*episode)
+
+
 
 #################################################################################
 # Main
 
 if __name__ == "__main__":
+
+    env = gym.make('ALE/SpaceInvaders-v5', render_mode='human')
+    env.reset()
+
+    state_shape = env.observation_space.shape
+    action_shape = env.action_space.n
+
+    model = network(state_shape, action_shape)
+    model.summary()
+
+    env.close()
+
+    ### Testing
     # print(env.unwrapped.get_action_meanings())
 
     # print(state_shape)
     # train()
     # preproces_plot()
 
-
     # print(env.action_space)
     # print(env.observation_space)
     # obs_preprocessed = preprocess(env.env)
     # plt.imshow(obs_preprocessed, cmap='gray')
     # plt.show()
-    env.close()
