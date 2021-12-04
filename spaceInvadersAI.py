ae5169a98c520cb8ee061fa64161c484e2e891c4
Zayaan Moez <zayaanmoez@outlook.com>
2021-12-04 15:26:21 -0500

    Checkpoints, MSE loss and VarainceScaling

--- spaceInvadersAI.py
+++ spaceInvadersAI.py
@@ -12,13 +12,13 @@ from itertools import islice
 # Hyperparameters
 
 BATCH_SIZE = 128
-REPLAY_SIZE = 1000
-EPISODES = 300
+REPLAY_SIZE = 2000
+EPISODES = 500
 TARGET_MODEL_UPDATE = 200
 REPLAY_MEMORY = 50_000
 
 # CNN model params
-LEARNING_RATE = 0.003
+LEARNING_RATE = 0.00025
 KERNEL_SIZE = [8,4,3]
 STRIDES = [4,2,1]
 POOL_SIZE = 2
@@ -103,7 +103,9 @@ def preproces_plot():
 
 def network(state_shape, action_shape):
     
-    initializer = keras.initializers.HeNormal()
+    #     initializer = keras.initializers.VarianceScaling(scale=2.0, mode='fan_in', distribution='truncated_normal')
+    #     initializer = keras.initializers.HeUniform()
+    initializer = keras.initializers.GlorotUniform()
     model = keras.Sequential()
 
     # Input layer
@@ -126,7 +128,7 @@ def network(state_shape, action_shape):
     # Output layer
     model.add(keras.layers.Dense(action_shape, activation='softmax'))
 
-    model.compile(loss=keras.losses.Huber(), optimizer=keras.optimizers.Adam(learning_rate=LEARNING_RATE), metrics=['accuracy'])
+    model.compile(loss=keras.losses.MeanSquaredError(), optimizer=keras.optimizers.Adam(learning_rate=LEARNING_RATE), metrics=[keras.metrics.MeanSquaredError()])
     
     return model
 
@@ -135,7 +137,7 @@ def network(state_shape, action_shape):
 #################################################################################
 # Training agent
 
-def train(env, replay_memory, model, target_model):
+def train(env, replay_memory, model, target_model, epoch):
 
     if len(replay_memory) <= REPLAY_SIZE:
         return
@@ -154,7 +156,7 @@ def train(env, replay_memory, model, target_model):
             # Bellman Equation : r(s) + gamma * max_a'(Q(s',a'))
             qValue = reward + DISCOUNT_FACTOR * np.max(succesive_q_values[i])
         else:
-            # Negative reward if lost a life
+            # Pick reward as the episode has ended; no succesive state
             qValue = -1
         
         # TODO: Figure out y_train values work or not
@@ -162,12 +164,31 @@ def train(env, replay_memory, model, target_model):
         # q_value_arr for a state s : [qVal action1, qval action1, ..., qval action18] 
         q_value_arr = q_values[i]
         # Qvalue for action a  : Q(s,a) + alpha(r(s) + gamma*max_a'(Q(s',a')) - Q(s, a))         
-        q_value_arr[action] = q_value_arr[action] + LEARNING_RATE * (qValue - q_value_arr[action])
+        # q_value_arr[action] = (1 - Q_LEARNING_RATE) * q_value_arr[action] + Q_LEARNING_RATE * qValue
+        q_value_arr[action] = qValue # q_value_arr[action] - LEARNING_RATE * (qValue - q_value_arr[action])
 
         X_train.append(state)
         Y_train.append(q_value_arr)
     
-    model.fit(np.array(X_train), np.array(Y_train), batch_size=BATCH_SIZE)
+    if epoch % 250 == 0:
+        checkpoint_filepath = "./tmp/cp.ckpt"
+
+        model_checkpoint_callback = keras.callbacks.ModelCheckpoint(
+            filepath=checkpoint_filepath,
+            save_weights_only=True,
+            save_best_only=True,
+            verbose=1,
+            save_freq=1,
+            monitor='mean_squared_error',
+            mode='min')
+
+        # Model weights are saved if it's the best seen so far.
+        model.fit(np.array(X_train), np.array(Y_train), batch_size=BATCH_SIZE, callbacks=[model_checkpoint_callback]) 
+    else:
+        model.fit(np.array(X_train), np.array(Y_train), batch_size=BATCH_SIZE)
+
+#     # The model weights (that are considered the best) are loaded into the model.
+#     model.load_weights(checkpoint_filepath)
 
 
 
@@ -175,6 +196,7 @@ def train(env, replay_memory, model, target_model):
 # Deep Q-Learning agent
 
 def DQN_agent(env):
+
     epsilon = 1
     eps_min = 0.05
     eps_max = 1
@@ -194,6 +216,7 @@ def DQN_agent(env):
 
     update_target_counter = 0
     step_counter = 0
+    epoch = 0
 
     for episode in range(EPISODES):
         state = env.reset()
@@ -201,7 +224,7 @@ def DQN_agent(env):
         done = False
         dead = False
         start_life = 3
-
+        
         state,_,_,_ = env.step(0)
         stacked_state, stacked_frames = stack_frames(stacked_frames, state, True)
 
@@ -226,8 +249,10 @@ def DQN_agent(env):
                 dead = True
                 start_life = info['lives']
 
+            replay_memory.append([stacked_state, action, reward, new_stacked_state, dead])
+
             if step_counter % 4 == 0 or done:
-                replay_memory.append([stacked_state, action, reward, new_stacked_state, dead])
+                epoch += 1
                 train(env, replay_memory, model, target_model)
 
             score += reward
@@ -239,7 +264,7 @@ def DQN_agent(env):
                     target_model.set_weights(model.get_weights())
 
             if done:
-                    print('Score: {} after epsidoe = {}'.format(score, episode))
+                print('Score: {} after epsidoe = {}'.format(score, episode))
 
         # Exponential Decay for epsilon (explore with atleast eps_min probability)
         epsilon = eps_min + (eps_max - eps_min) * np.exp(-decay * episode)
@@ -285,7 +310,7 @@ if __name__ == "__main__":
 # Testing the model performance
 
 def test():
-    env = gym.make('ALE/SpaceInvaders-v5', render_mode='human')
+    env = gym.make('SpaceInvaders-v4, render_mode='human')
     state = env.reset()
 
     TEST_EPISODES = 100
